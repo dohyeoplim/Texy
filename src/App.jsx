50ba46b8608b8dbf1c9f5b285bcc9436ebd407c6
Dohyeop Lim <dhlim0817@gmail.com>
2025-03-22 01:45:25 +0900

    Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import TableGenerator from "./components/main/TableGenerator";
+import Header from "./components/common/Header";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
+        <div className="min-h-screen antialiased">
+            <div className="w-full max-w-2xl mx-auto px-4 sm:px-6 md:px-8">
+                <Header />
+                <main>
+                    <TableGenerator />
+                </main>
+                <footer className="container mx-auto px-4 py-6 text-center text-gray-600 dark:text-gray-400 text-sm">
+                    <p>© {new Date().getFullYear()} Dohyeop Lim</p>
+                </footer>
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        </div>
     );
 }
 
