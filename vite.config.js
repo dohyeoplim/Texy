84db2051e59528996a9df7f38264a906291fe2f6
Dohyeop Lim <dhlim0817@gmail.com>
2025-03-21 22:47:22 +0900

    Configure vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "./src"),
+        },
+    },
+    worker: {
+        format: "es",
+    },
+    optimizeDeps: {
+        exclude: ["comlink"],
+    },
+    build: {
+        target: "esnext",
+        outDir: "dist",
+        assetsDir: "assets",
+        sourcemap: true,
+    },
+    server: {
+        port: 5000,
+        open: true,
+        cors: true,
+    },
 });
